a7e258511b8b814d2b8909c7590b22a200d462b7
Artem Vlakh <vlakhartem@gmail.com>
2022-09-15 15:29:13 +0200

    landing draft done

--- assets/script/main.dev.js
+++ assets/script/main.dev.js
@@ -12,4 +12,20 @@ function headerScroll() {
   });
 }
 
-headerScroll();
+headerScroll(); // ACCORDION
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
--- assets/script/main.js
+++ assets/script/main.js
@@ -13,3 +13,20 @@ function headerScroll(){
 }
 
 headerScroll();
+
+// ACCORDION
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+        } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
